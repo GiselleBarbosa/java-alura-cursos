1491e57b4b02d4db7947bd15111aa2a56c62cc1b
GiselleBarbosa <g88barbosa@gmail.com>
2024-05-16 13:24:37 -0300

    adicionado tratamento de exceção na classe main

--- src/main.java
+++ src/main.java
@@ -2,7 +2,13 @@ public class main {
     public static void main(String[] args) {
         ConsultaCep consultaCep = new ConsultaCep();
 
-        Endereco novoEndereco =  consultaCep.buscaEndereco("02810000");
-        System.out.println(novoEndereco);
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
+            System.out.println(novoEndereco);
+
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
     }
 }
