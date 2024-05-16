40a1e1c4ba24a3f1d30b907f3e33bc43b698f2e0
GiselleBarbosa <g88barbosa@gmail.com>
2024-05-16 13:41:24 -0300

    implementado gerador de arquivo json

--- src/main.java
+++ src/main.java
@@ -1,14 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         ConsultaCep consultaCep = new ConsultaCep();
 
+        System.out.println("Digite um numero de CEP para consultar: ");
+        var cep = input.nextLine();
+
         try {
-            Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
             System.out.println(novoEndereco);
+            GeradorDeArquivo geradorDeArquivo = new GeradorDeArquivo();
+            geradorDeArquivo.criarArquivoJson(novoEndereco);
 
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
             System.out.println("Finalizando a aplicação");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
