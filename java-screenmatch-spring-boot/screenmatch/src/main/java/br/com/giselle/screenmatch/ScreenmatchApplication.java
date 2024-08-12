fbbfc821d0a1d160d6ac0db63693afa50c6456c0
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-12 17:21:03 -0300

    remover PokeApi

--- java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/ScreenmatchApplication.java
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,4 @@
 package br.com.giselle.screenmatch;
-
-import br.com.giselle.screenmatch.models.DadosPokemon;
 import br.com.giselle.screenmatch.models.DadosSerie;
 import br.com.giselle.screenmatch.services.ConsumoApi;
 import br.com.giselle.screenmatch.services.ConverteDados;
@@ -8,42 +6,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
-
     @Value("${OMDB_API_KEY}")
     String apiKey;
-
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
     }
-
     @Override
     public void run(String... args) throws Exception {
         var consumoApi = new ConsumoApi();
-        var conversor = new ConverteDados();
-
         // Chamada da API OmDb
-        var jsonSerie = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=" + apiKey);
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=" + apiKey);
 
-        System.out.println("\n****** SAÍDA DO JSON COMPLETO DA API OMDB ******");
-        System.out.println(jsonSerie);
+        System.out.println("\n****** SAÍDA DO JSON COMPLETO ******");
+        System.out.println(json);
 
-        DadosSerie dadosSerie = conversor.obterDados(jsonSerie, DadosSerie.class);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-        System.out.println("\n****** SAÍDA APÓS CONVERSÃO DA API OMDB ******");
-        System.out.println("Título: " + dadosSerie.titulo());
-        System.out.println("Total de Temporadas: " + dadosSerie.totalTemporadas());
-        System.out.println("Avaliação IMDb: " + dadosSerie.avaliacao());
-
-        // Chamada da API PokeApi
-        var jsonPokemon = consumoApi.obterDados("https://pokeapi.co/api/v2/pokemon/mew");
-
-        DadosPokemon dadosPokemon = conversor.obterDados(jsonPokemon, DadosPokemon.class);
-
-        System.out.println("\n****** SAÍDA APÓS CONVERSÃO DA API POKEAPI ******");
-        System.out.println(dadosPokemon);
-
+        System.out.println("\n****** SAÍDA APÓS CONVERSÃO ******");
+        System.out.println("Título: " + dados.titulo());
+        System.out.println("Total de Temporadas: " + dados.totalTemporadas());
+        System.out.println("Avaliação IMDb: " + dados.avaliacao());
     }
 }
