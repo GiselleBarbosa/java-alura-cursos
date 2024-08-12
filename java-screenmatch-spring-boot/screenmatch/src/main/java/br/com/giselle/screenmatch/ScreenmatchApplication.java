6492505f0101fee0d4f54a3150b82585727b26cc
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-12 17:40:37 -0300

    implementar classe DadosEpisodio

--- java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/ScreenmatchApplication.java
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/ScreenmatchApplication.java
@@ -1,4 +1,5 @@
 package br.com.giselle.screenmatch;
+import br.com.giselle.screenmatch.models.DadosEpisodio;
 import br.com.giselle.screenmatch.models.DadosSerie;
 import br.com.giselle.screenmatch.services.ConsumoApi;
 import br.com.giselle.screenmatch.services.ConverteDados;
@@ -23,15 +24,25 @@ public class ScreenmatchApplication implements CommandLineRunner {
         // Chamada da API OmDb
         var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=" + apiKey);
 
-        System.out.println("\n****** SAÍDA DO JSON COMPLETO ******");
-        System.out.println(json);
-
         ConverteDados conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
+
+        System.out.println("\n" + dados);
         System.out.println("\n****** SAÍDA APÓS CONVERSÃO ******");
         System.out.println("Título: " + dados.titulo());
         System.out.println("Total de Temporadas: " + dados.totalTemporadas());
         System.out.println("Avaliação IMDb: " + dados.avaliacao());
+
+        json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=7&apikey=" + apiKey);
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+
+
+        System.out.println("\n" + dadosEpisodio);
+        System.out.println("\n****** SAÍDA APÓS CONVERSÃO ******");
+        System.out.println("Título: " + dadosEpisodio.titulo());
+        System.out.println("Número do episódio: " + dadosEpisodio.numeroEpisodio());
+        System.out.println("Avaliação IMDb: " + dadosEpisodio.avaliacao());
+        System.out.println("Data de lançamento: " + dadosEpisodio.dataLancamento());
     }
 }
