4b01a7bd7ff79b756239db9e80e503ac0370afb0
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-12 15:20:53 -0300

    implementar conversão de dados da API OmDb para objeto DadosSerie

--- java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/ScreenmatchApplication.java
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.giselle.screenmatch;
 
+import br.com.giselle.screenmatch.models.DadosSerie;
 import br.com.giselle.screenmatch.services.ConsumoApi;
+import br.com.giselle.screenmatch.services.ConverteDados;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,12 +23,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
         var consumoApi = new ConsumoApi();
 
         // Chamada da API OmDb
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=" + apiKey);
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=" + apiKey);
+
+        System.out.println("");
+        System.out.println("******SAIDA DO JSON COMPLETO*********");
         System.out.println(json);
 
-        // Chamada da API PokeApi
-        var json2 = consumoApi.obterDados("https://pokeapi.co/api/v2/pokemon/mew");
-        System.out.println(json2);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
+        System.out.println("");
+        System.out.println("******SAIDA APOS CONVERSAO*********");
+        System.out.println(dados);
     }
 }
