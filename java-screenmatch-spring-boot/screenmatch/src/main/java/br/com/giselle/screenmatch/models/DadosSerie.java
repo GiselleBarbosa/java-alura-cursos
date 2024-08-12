0d396002fb158bdf61adf15de2ec70d6c9a393d5
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-12 13:55:20 -0300

    adiciona jackson-databind ao pom e cria DadosSerieModel

---
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/models/DadosSerie.java
@@ -0,0 +1,8 @@
+package br.com.giselle.screenmatch.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
