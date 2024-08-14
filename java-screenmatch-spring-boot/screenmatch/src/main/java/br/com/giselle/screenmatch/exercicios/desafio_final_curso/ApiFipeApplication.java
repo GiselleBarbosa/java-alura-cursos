872336c0f14320032b168517d5e227c13c18b4b1
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-13 23:38:04 -0300

    wip: implementar switch case para tipo de veiculo

--- java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/ApiFipeApplication.java
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/ApiFipeApplication.java
@@ -1,21 +1,22 @@
 package br.com.giselle.screenmatch.exercicios.desafio_final_curso;
 
-import br.com.giselle.screenmatch.ScreenmatchApplication;
-import br.com.giselle.screenmatch.principal.Principal;
+import br.com.giselle.screenmatch.exercicios.desafio_final_curso.core.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApiFipeApplication implements CommandLineRunner {
     @Autowired
-    private Principal principal;
+    private Main main;
 
     public static void main(String[] args) {
-        SpringApplication.run(ScreenmatchApplication.class, args);
+        SpringApplication.run(ApiFipeApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        principal.exibeMenu();
+        main.exibeMenu();
     }
 }
--- java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/core/Main.java
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/core/Main.java
@@ -2,9 +2,11 @@ package br.com.giselle.screenmatch.exercicios.desafio_final_curso.core;
 
 import br.com.giselle.screenmatch.services.ConsumoApi;
 import br.com.giselle.screenmatch.services.ConverteDados;
+import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+@Component
 public class Main {
     private final Scanner scanner = new Scanner(System.in);
 
@@ -12,7 +14,35 @@ public class Main {
     private final ConverteDados conversor = new ConverteDados();
 
     private static final String API_FIPE = "https://parallelum.com.br/fipe/api/v1/";
+    private String tipoVeiculoSelecionado;
 
-    public void exibeMenu() {    }
+    public void exibeMenu() {
+        String tipoVeiculoSelecionado = null;
+
+        while (tipoVeiculoSelecionado == null) {
+            System.out.println("\n\n########## BEM VINDO(A) AO SISTEMA DE CONSULTA À TABELA FIPE ########## ");
+            System.out.print("\n\nESCOLHA UM TIPO DE VEICULO. DIGITE A OPÇÃO DESEJADA: ");
+            System.out.println("\n[1] Carros \n[2] Motos \n[3] Caminhoes");
+
+            var tiposVeiculo = scanner.nextLine();
+
+            switch (tiposVeiculo) {
+                case "1":
+                    tipoVeiculoSelecionado = "carros";
+                    break;
+                case "2":
+                    tipoVeiculoSelecionado = "motos";
+                    break;
+                case "3":
+                    tipoVeiculoSelecionado = "caminhoes";
+                    break;
+                default:
+                    System.out.println("Opção inválida! Por favor, digite 1, 2 ou 3.");
+            }
+        }
+
+        System.out.println("Você selecionou: " + tipoVeiculoSelecionado);
+    }
+
 
 }
