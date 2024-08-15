284c9b5737999a270bf05b34571e9c4ea119ee9d
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-14 22:07:10 -0300

    refac: remove anotacao nao utilizada

--- java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/model/DadosVeiculos.java
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/model/DadosVeiculos.java
@@ -1,12 +1,11 @@
 package br.com.giselle.screenmatch.exercicios.desafio_final_curso.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public record DadosVeiculos(
-        @JsonAlias("name") String name,
-        @JsonAlias("code") String code
+        String codigo,
+        String nome
 ) {
 }
