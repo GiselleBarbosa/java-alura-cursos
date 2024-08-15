fae6d923bde32855bfd0e41b4da92dece7722def
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-14 22:03:02 -0300

    feat: Implementa classe ConverteDados para conversão de JSON em objetos Java
    - Adiciona a classe ConverteDados com métodos para converter JSON em objetos Java individuais e listas de objetos.
    - Método `obterDados`: converte uma string JSON em um objeto da classe especificada.
    - Método `obterLista`: converte uma string JSON em uma lista de objetos da classe especificada.
    - Usa a biblioteca Jackson `ObjectMapper` para realizar a conversão.

---
+++ java-screenmatch-spring-boot/screenmatch/src/main/java/br/com/giselle/screenmatch/exercicios/desafio_final_curso/services/IConverteDados.java
@@ -0,0 +1,9 @@
+package br.com.giselle.screenmatch.exercicios.desafio_final_curso.services;
+
+import java.util.List;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+
+    <T> List<T> obterLista(String json, Class<T> classe);
+}
