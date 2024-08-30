ce62bd82f060dd003b026d4c6ca9483cfb0111a9
GiselleBarbosa <g88barbosa@gmail.com>
2024-08-29 22:57:11 -0300

    Refatora NewOrderMain para usar a nova classe KafkaDispatcher
    - Abstrai a lógica de envio de mensagens para Kafka em uma nova classe `KafkaDispatcher`.
    - Implementa `KafkaDispatcher` que encapsula a configuração do `KafkaProducer` e o método `send` para enviar mensagens para um tópico específico.
    - Refatora a classes `NewOrderMain, EmailService e FraudDetectorService` para usar a nova classe `KafkaDispatcher`, simplificando o código e melhorando a reutilização.
    - Utiliza o método `send` do `KafkaDispatcher` para enviar pedidos e emails, melhorando a legibilidade e a manutenção do código.
    - Garante que o `KafkaProducer` seja fechado adequadamente usando o bloco try-with-resources no `NewOrderMain`.

--- java-kafka-ecommerce/src/main/java/com/giselle/ecommerce/NewOrderMain.java
+++ java-kafka-ecommerce/src/main/java/com/giselle/ecommerce/NewOrderMain.java
@@ -1,46 +1,20 @@
 package com.giselle.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
 
-        for (var i = 0; i < 10; i++) {
+                var key = UUID.randomUUID().toString();
+                var value = key + ", 62242, 785985545245";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
 
-            var key = UUID.randomUUID().toString();
-            var value = key + ", 62242, 785985545245";
-            var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
-
-            Callback callback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace(System.err);
-                    return;
-                }
-                System.out.println("Sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-            };
-
-            var email = "Recebemos seu pedido, e ele já está sendo processado!";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-
-            producer.send(record, callback).get();
-            producer.send(emailRecord, callback).get();
+                var email = "Recebemos seu pedido, e ele já está sendo processado!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
         }
-    }
-
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
     }
 }
